fb2497f5c81423f109bb1ee2364f1c784044b063
ACE-DVLPT <alexis.cousture@gmail.com>
2020-03-05 19:43:13 +0100

    [SHARE] Don't use this commit

--- Mareu/app/src/main/java/fr/ace/mareu/ui/MeetingsListActivity.java
+++ Mareu/app/src/main/java/fr/ace/mareu/ui/MeetingsListActivity.java
@@ -3,14 +3,31 @@ package fr.ace.mareu.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import fr.ace.mareu.R;
 
 public class MeetingsListActivity extends AppCompatActivity {
 
+    @BindView(R.id.activity_meetings_list_btn_add_meeting)
+    FloatingActionButton mBtnAddMeeting;
+    @BindView(R.id.fragment_meeting_txt_line1)
+    TextView mTxtLine1;
+    @BindView(R.id.fragment_meeting_txt_line2)
+    TextView mTxtLine2;
+    @BindView(R.id.fragment_meeting_colored_circle)
+    ImageView mColoredCircle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_meetings_list);
+        ButterKnife.bind(this);
+
     }
 }

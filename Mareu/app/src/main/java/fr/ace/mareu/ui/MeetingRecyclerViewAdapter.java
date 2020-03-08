3333f217f99412b309f2f8e6743f1fa26c1fec05
ACE-DVLPT <alexis.cousture@gmail.com>
2020-03-08 21:55:35 +0100

    [FEAT] ViewHolder -> ok

--- Mareu/app/src/main/java/fr/ace/mareu/ui/MeetingRecyclerViewAdapter.java
+++ Mareu/app/src/main/java/fr/ace/mareu/ui/MeetingRecyclerViewAdapter.java
@@ -2,9 +2,15 @@ package fr.ace.mareu.ui;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import fr.ace.mareu.R;
 
 public class MeetingRecyclerViewAdapter extends RecyclerView.Adapter<MeetingRecyclerViewAdapter.ViewHolder> {
 
@@ -28,8 +34,18 @@ public class MeetingRecyclerViewAdapter extends RecyclerView.Adapter<MeetingRecy
     // ViewHolder
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.fragment_meeting_colored_circle)
+        ImageView mImageViewColoredCircle;
+        @BindView(R.id.fragment_meeting_delete_button)
+        ImageView mImageViewDeleteButton;
+        @BindView(R.id.fragment_meeting_txt_line1)
+        TextView mTextViewLine1;
+        @BindView(R.id.fragment_meeting_txt_line2)
+        TextView mTextViewLine2;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }

40e1e9e11bc9cfa17389eca99cfe5ac671fe7f0a
ACE-DVLPT <alexis.cousture@gmail.com>
2020-03-08 22:56:39 +0100

    [FEAT] Class DI added

---
+++ Mareu/app/src/main/java/fr/ace/mareu/di/DI.java
@@ -0,0 +1,21 @@
+package fr.ace.mareu.di;
+
+import fr.ace.mareu.api.ApiService;
+import fr.ace.mareu.api.FakeApiService;
+
+/**
+ * Dependency injector to get instance of services
+ */
+public class DI {
+
+    private static ApiService service = new FakeApiService();
+
+    /**
+     * Get an instance on ApiService
+     * @return
+     */
+    public static ApiService getApiService(){
+        return service;
+    }
+
+}

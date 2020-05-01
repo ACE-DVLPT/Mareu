2b8e701372b1ede9daaf7c7240c0db3496f0cb29
ACE-DVLPT <alexis.cousture@gmail.com>
2020-05-01 18:12:13 +0200

    [TESTS] some tests added

--- Mareu/app/src/androidTest/java/fr/ace/mareu/ui/MeetingsListActivityTest.java
+++ Mareu/app/src/androidTest/java/fr/ace/mareu/ui/MeetingsListActivityTest.java
@@ -1,10 +1,23 @@
 package fr.ace.mareu.ui;
 
+import android.content.pm.ActivityInfo;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.DatePicker;
+
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,16 +28,20 @@ import java.util.Arrays;
 import java.util.Calendar;
 
 import fr.ace.mareu.R;
-import fr.ace.mareu.api.ApiService;
 import fr.ace.mareu.model.Meeting;
 import fr.ace.mareu.utils.DeleteViewAction;
 import fr.ace.mareu.utils.RecyclerViewItemCountViewAssertion;
-import fr.ace.mareu.utils.di.DI;
 
 import static androidx.test.espresso.Espresso.onView;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
@@ -44,9 +61,6 @@ public class MeetingsListActivityTest {
         refreshRecycleViewAdapter();
     }
 
-    /**
-     * To run "refreshRecycleViewAdapter" method with the right thread
-     */
     public void refreshRecycleViewAdapter(){
         try {
             runOnUiThread(new Runnable() {
@@ -61,9 +75,6 @@ public class MeetingsListActivityTest {
         }
     }
 
-    /**
-     * Test if the right message is displayed when the recyclerView is empty
-     */
     @Test
     public void recyclerView_whenIsEmpty_displayTheRightMessage() {
 
@@ -72,9 +83,102 @@ public class MeetingsListActivityTest {
 
     }
 
-    /**
-     * Test when a delete button is clicked, the item should be no longer displayed
-     */
+    @Test
+    public void filterMenu_whenPlaceFilterSelected_theCorrectDialogMustBeDisplayed(){
+        ViewInteraction actionMenuItemView = onView(
+                Matchers.allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.activity_meetings_list_toolbar),
+                                1),
+                        0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction materialTextView = onView(
+                Matchers.allOf(withId(R.id.title), withText("Filtre par salle"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        // test
+        onView(allOf(ViewMatchers.withId(R.id.fragment_dialog_filter_place_spinner_place))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void filterMenu_whenDateFilterSelected_theCorrectDialogMustBeDisplayed(){
+        ViewInteraction actionMenuItemView = onView(
+                Matchers.allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.activity_meetings_list_toolbar),
+                                1),
+                        0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction materialTextView = onView(
+                Matchers.allOf(withId(R.id.title), withText("Filtre par date"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        // test
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void filterMenu_whenFilterIsAdded_TheChipGroupMustBeDisplayed(){
+        // Before does not exist
+        ViewInteraction viewGroup = onView(
+                Matchers.allOf(withId(R.id.activity_meetings_list_chip_group_filters),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        viewGroup.check(doesNotExist());
+
+        // Add chip group
+        ViewInteraction actionMenuItemView = onView(
+                Matchers.allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.activity_meetings_list_toolbar),
+                                1),
+                        0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction materialTextView = onView(
+                Matchers.allOf(withId(R.id.title), withText("Filtre par salle"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                Matchers.allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.buttonPanel),
+                                        0),
+                                3)));
+        materialButton.perform(scrollTo(), click());
+
+        // Chip group is displayed
+        viewGroup.check(matches(isDisplayed()));
+    }
+
     @Test
     public void recyclerView_whenDeleteButtonIsClicked_itemShouldBeRemoved(){
 
@@ -113,4 +217,73 @@ public class MeetingsListActivityTest {
 
     }
 
+    @Test
+    public void landScapeView_whenMeetingIsClicked_memberEmailsMustBeDisplayed(){
+        Calendar meetingDATE = Calendar.getInstance();
+        Calendar meetingDuration = Calendar.getInstance();
+        meetingDuration.set(Calendar.YEAR, 0);
+        meetingDuration.set(Calendar.MONTH, 0);
+        meetingDuration.set(Calendar.DAY_OF_MONTH, 0);
+        meetingDuration.set(Calendar.HOUR, 1);
+        meetingDuration.set(Calendar.HOUR_OF_DAY, 0);
+        meetingDuration.set(Calendar.MINUTE, 45);
+        meetingDuration.set(Calendar.SECOND, 0);
+        meetingDuration.set(Calendar.MILLISECOND, 0);
+        Meeting meeting = new Meeting(
+                "topic",
+                "Peach",
+                meetingDATE,
+                meetingDuration,
+                new ArrayList<>(Arrays.asList(
+                        ("email@email.com")
+                ))
+        );
+
+        mActivityTestRule.getActivity().mApiService.addMeeting(meeting);
+        refreshRecycleViewAdapter();
+        mActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        onView(allOf(withId(R.id.recyclerview_meetings_list),isDisplayed()))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click() ));
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         // test
+        onView(allOf(ViewMatchers.withId(R.id.fragment_member_list_text_view), isDisplayed()))
+                .check(matches(withText("email@email.com")));
+    }
+
+    @Test
+    public void activity_whenAddFloatingButtonClicked_startMeetingCreatorActivity(){
+        Intents.init();
+
+        onView(allOf(withId(R.id.activity_meetings_list_btn_add_meeting),isDisplayed()))
+                .perform(click());
+
+        // Ensure that the correct activity has started
+        intended(hasComponent(MeetingCreatorActivity.class.getName()));
+
+        Intents.release();
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
